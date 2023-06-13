64eb934335946af6c43f7ba7fa8b9dce18381223
강준 <keisoft.kjun@gmail.com>
2023-06-13 19:52:46 +0900

    초기 커밋.

---
+++ EFTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new MyDbContext())
+            {
+                db.Database.EnsureCreated();
+
+                var employee = new Employee { Name = "Kang Jun", Age = 40 };
+                db.Employees.Add(employee);
+                db.SaveChanges();
+            }
+        }
+
+        public class Employee
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public int Age { get; set; }
+        }
+
+        public class MyDbContext : DbContext
+        {
+            public DbSet<Employee> Employees { get; set; }
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlite("Filename=mydb.db");
+            }
+        }
+    }
+}
